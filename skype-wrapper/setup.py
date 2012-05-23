c9dda7c099f1ad4c4608fe9f86e29000f3a5aa51
Christian Rupp <christian@christian-Notebook-PC.(none)>
2012-05-23 16:48:33 +0200

    changes in options

--- skype-wrapper/setup.py
+++ skype-wrapper/setup.py
@@ -52,7 +52,7 @@ setup(name='skype-wrapper',
       data_files=[('share/glib-2.0/schemas', ['data/schemas/apps.skype-wrapper.gschema.xml']),
         ('share/applications', ['data/applications/skype-wrapper.desktop', 'data/applications/skype-wrapper-settings.desktop']),
         ('share/pixmaps', ['data/pixmaps/skype-wrapper.svg']),
-        ('share/skype-wrapper', ['src/helpers.py', 'src/indicator-applet-skype','src/indicator-applet-skype.py','src/postinst.py','src/settings.py','src/shared.py','src/skype-wrapper.py','src/unitylauncher.py','src/uisettings.py']),
+        ('share/skype-wrapper', ['src/helpers.py', 'src/indicator-applet-skype','src/indicator-applet-skype.py','src/postinst.py','src/settings.py','src/shared.py','src/skype-wrapper.py','src/unitylauncher.py','src/uisettings.py','src/uninstallsni.py']),
         ('share/skype-wrapper/icons', ['src/icons/skype-24.svg','src/icons/skype-wrapper-16.png','src/icons/skype-wrapper-22.png','src/icons/skype-wrapper-24.png','src/icons/skype-wrapper-48.svg'])
       ],
       cmdclass={'install_data': InstallData},#, 'uninstall': Uninstall},
--- skype-wrapper/src/uisettings.py
+++ skype-wrapper/src/uisettings.py
@@ -1,120 +1,114 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; tab-width: 4; mode: python -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: t -*-
-# vi: set ft=python sts=4 ts=4 sw=4 noet 
+# vi: set ft=python sts=4 ts=4 sw=4 noet
 #
-# Copyright 2011 Shannon Black
+# Copyright 2012 Shannon Black
 #
 # Authors:
 #    Christian Rupp <christian@r-k-r.de>
 #
-# This program is free software: you can redistribute it and/or modify it 
+# This program is free software: you can redistribute it and/or modify it
 # under the terms of either or both of the following licenses:
 #
-# 1) the GNU Lesser General Public License version 3, as published by the 
+# 1) the GNU Lesser General Public License version 3, as published by the
 # Free Software Foundation; and/or
-# 2) the GNU Lesser General Public License version 2.1, as published by 
+# 2) the GNU Lesser General Public License version 2.1, as published by
 # the Free Software Foundation.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
-# PURPOSE.  See the applicable version of the GNU Lesser General Public 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the applicable version of the GNU Lesser General Public
 # License for more details.
 #
-# You should have received a copy of both the GNU Lesser General Public 
-# License version 3 and version 2.1 along with this program.  If not, see 
+# You should have received a copy of both the GNU Lesser General Public
+# License version 3 and version 2.1 along with this program.  If not, see
 # <http://www.gnu.org/licenses/>
 #
 
 from gi.repository import Gtk, Gio
 import settings
+import subprocess
 
 BASE_KEY = "apps.skype-wrapper"
 setting = Gio.Settings.new(BASE_KEY)
 
-class uisettings(Gtk.Window):
-	
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Settings")
-		table = Gtk.Table(10, 5, True)
-        self.add(table)
-        
-        #All of the labels is their a better way to code it?        	
-		Lnouoc = Gtk.Label("Send notifications when someone goes on or offline")
-		table.attach(Lnouoc, 0,4, 0, 1)
-		Snouoc = Gtk.Switch()
-		table.attach(Snouoc, 4,5, 0, 1)
-		
-		setting.bind("notify-on-useronlinestatuschange", Snouoc, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lnomr = Gtk.Label("Send notifications when you recieve a message")
-		table.attach(Lnomr, 0, 4, 1, 2)
-		Snomr = Gtk.Switch()
-		table.attach(Snomr, 4,5, 1, 2)
-		
-		setting.bind("notify-on-messagerecieve", Snomr, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lnoi = Gtk.Label("Send on startup notifications about all online contacts")
-		table.attach(Lnoi, 0, 4, 2, 3)
-		Snoi = Gtk.Switch()
-		table.attach(Snoi, 4,5, 2, 3)
-		
-		setting.bind("notify-on-initializing", Snoi, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Ldia = Gtk.Label("Show avatars next to the indicator message")
-		table.attach(Ldia, 0, 4, 3, 4)
-		Sdia = Gtk.Switch()
-		table.attach(Sdia, 4,5, 3, 4)
-		
-		setting.bind("display-indicator-avatars", Sdia, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Ldna = Gtk.Label("Show avatars in the notifications")
-		table.attach(Ldna, 0, 4, 4, 5)
-		Sdna = Gtk.Switch()
-		table.attach(Sdna, 4,5, 4, 5)
-		
-		setting.bind("display-notification-avatars", Sdna, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lnoift = Gtk.Label("Notify on incoming filetransfer")
-		table.attach(Lnoift, 0, 4, 5, 6)
-		Snoift = Gtk.Switch()
-		table.attach(Snoift, 4,5, 5, 6)
-		
-		setting.bind("notify-on-incoming-filetransfer", Snoift, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lnooft = Gtk.Label("Notify on outgoing filetransfer")
-		table.attach(Lnooft, 0, 4, 6, 7)
-		Snooft = Gtk.Switch()
-		table.attach(Snooft, 4,5, 6, 7)
-		
-		setting.bind("notify-on-outgoing-filetransfer", Snooft, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lsoofp = Gtk.Label("Show outgoing filetransfer-progress in the Launcher")
-		table.attach(Lsoofp, 0, 4, 7, 8)
-		Ssoofp = Gtk.Switch()
-		table.attach(Ssoofp, 4,5, 7, 8)
-		
-		setting.bind("show-outgoing-file-progress", Ssoofp, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lsifp = Gtk.Label("Show incoming filetransfer-progress in the Launcher")
-		table.attach(Lsifp, 0, 4, 8, 9)
-		Ssifp = Gtk.Switch()
-		table.attach(Ssifp, 4,5, 8, 9)
-		
-		setting.bind("show-incoming-file-progress", Ssifp, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-		Lugs = Gtk.Label("Use the global online status of the system")
-		table.attach(Lugs, 0, 4, 9, 10)
-		Sugs = Gtk.Switch()
-		table.attach(Sugs, 4,5, 9, 10)
+class DialogAdvanced(Gtk.Dialog):
 				
-		setting.bind("use-global-status", Sugs, "active", Gio.SettingsBindFlags.DEFAULT)
-		
-	def printit(self, blaa):
- 		print(str(blaa))
- 		
+		def __init__(self, parent):		
+				Gtk.Dialog.__init__(self, "Remove sni-qt", parent, 0,(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,Gtk.STOCK_OK, Gtk.ResponseType.OK))
+
+        		self.set_default_size(300, 100)
+
+        		label = Gtk.Label("This will remove sni-qt. The Skype-Icon will not be shown in the panel any longer. This may also affects other applications. To revert the change reinstall sni-qt in the software-center")
+				label.set_line_wrap(True)
+        		box = self.get_content_area()
+       			box.add(label)
+       			self.show_all()
+
+class uisettings(Gtk.Window):
+        def switch(self, table, label, option):
+                self.row += 1
+                gtk_label = Gtk.Label ( label )
+                gtk_label.set_alignment(0.0,0.5)
+
+                table.attach ( gtk_label, 0, 3, self.row, self.row+1, xpadding=10, ypadding = 2 )
+                gtk_switch = Gtk.Switch()
+                table.attach ( gtk_switch, 3, 4, self.row, self.row+1, xpadding=10, ypadding = 2 )
+                setting.bind( option, gtk_switch, "active", Gio.SettingsBindFlags.DEFAULT )
+
+        def __init__(self):
+                Gtk.Window.__init__(self, title="Skype Wrapper Options")
+                table = Gtk.Table(2, 4, True)
+                table.set_row_spacings(5)
+                table.set_col_spacings(1)
+                self.row = -1
+        		self.add(table)
+        		
+        		
+                self.switch( table, "Notify when someone goes on or offline", "notify-on-useronlinestatuschange")
+                self.switch( table, "Notify when you recieve a message","notify-on-messagerecieve")
+                self.switch( table, "Notify about all online contacts during startup", "notify-on-initializing")
+                self.switch( table, "Display avatars next to indicator message", "display-indicator-avatars")
+                self.switch( table, "Display avatars in the notifications", "display-notification-avatars")
+                self.switch( table, "Notify on incoming File Transfer", "notify-on-incoming-filetransfer" )
+                self.switch( table, "Notify on outgoing File Transfer", "notify-on-outgoing-filetransfer" )
+                self.switch( table, "Show outgoing File Transfer progress in the Launcher", "show-outgoing-file-progress" )
+                self.switch( table, "Show incoming File Transfer progress in the Launcher", "show-incoming-file-progress" )
+                self.switch( table, "Use the global online status of the system", "use-global-status" )
+
+                gtk_btn_adv = Gtk.Button("Remove the panel icon")
+                gtk_btn = Gtk.Button("Close")
+                gtk_btn.connect("clicked", Gtk.main_quit)
+                gtk_btn_adv.connect("clicked", self.on_advanced_clicked)
+
+                self.row += 1
+                table.attach ( gtk_btn, 3, 4, self.row, self.row+1,xpadding=10, ypadding = 2 )
+                table.attach ( gtk_btn_adv, 0, 2, self.row, self.row+1,xpadding=10, ypadding = 2 )
+
+
+
+		def on_advanced_clicked(self, widget):
+        		dialog = DialogAdvanced(self)
+        		response = dialog.run()
+
+        		if response == Gtk.ResponseType.OK:
+           			subprocess.call(['gksudo','python uninstallsni.py'])
+
+           			           			
+        		elif response == Gtk.ResponseType.CANCEL:
+            		print "The Cancel button was clicked"
+
+        		dialog.destroy()
+
+
 win = uisettings()
+win.set_border_width(10)
 win.connect("delete-event", Gtk.main_quit)
+win.connect("destroy", Gtk.main_quit)
 win.show_all()
+
 Gtk.main()
+
+
